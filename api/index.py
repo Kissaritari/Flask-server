9140be49c4cb562d8d3e5149292b2d59fde58f4c
kissa <saku.ylimaki@seamk.fi>
2024-02-14 10:50:01 +0200

    muutettu pythontiedosto

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify, request, json
 
 app = Flask(__name__)
+
+moods_data = []
 
 @app.route('/')
 def home():
@@ -9,3 +11,29 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/send_moods', methods=['POST'])
+def send_moods():
+    try:
+        data = request.get_json(force=True)
+        moods = data['moods']
+
+        if moods is None:
+            return jsonify({"error":"Wrong data"},400)
+        
+        moods_data.append(moods)
+        return jsonify({"message": "Mood sent successfully"}), 200
+    
+    except Exception as e:
+        return jsonify({"error":str(e)}),500
+    
+@app.route('/get_moods', methods=['GET'])    
+def hae_keskiarvo():
+    try:
+        if not moods_data:
+            return jsonify({"message": "Ei dataa saatavilla"}), 200
+
+        return jsonify({moods_data}), 200
+
+    except Exception as e:
+        return jsonify({"error":str(e)}, 500)
